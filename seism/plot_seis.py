7257f16bf3e67dac641c427445708cad474d1124
shubham-688 <agr.shubh.agr@gmail.com>
2025-11-10 14:49:20 -0500

    colour change

--- seism/plot_seis.py
+++ seism/plot_seis.py
@@ -7,6 +7,8 @@ st=read('220910_234700_AK.DHY..BHZ.sac')
 st1=read('220910_234700_AK.DIV..BHZ.sac')
 st2=read('220910_234700_AK.A22K..BHZ.sac')
 
-st2.filter('bandpass', freqmin=0.1, freqmax=.25)
+st_all=read('220910_*..BHZ.sac')
+
+st_all.filter('bandpass', freqmin=0.05, freqmax=.3)
 # st.plot(color='ivory',bgcolor='black', orientation="horizontal", linewidth=.5)
-st2.plot(color='lightpink',bgcolor='grey', orientation="horizontal", linewidth=.85)
+st_all.plot(color='slateblue',bgcolor='linen', orientation="horizontal", linewidth=2)
