dddbe99332c408c733b2b2554d335e705c74eb42
shubham-688 <agr.shubh.agr@gmail.com>
2026-01-16 12:24:02 +0530

    seis

--- seism/plot_seis.py
+++ seism/plot_seis.py
@@ -8,7 +8,11 @@ st1=read('220910_234700_AK.DIV..BHZ.sac')
 st2=read('220910_234700_AK.A22K..BHZ.sac')
 
 st_all=read('220910_*..BHZ.sac')
+st_all=read('220510_*..BHZ.sac')
+st_all=read('211*..BHZ.sac')
 
-st_all.filter('bandpass', freqmin=0.05, freqmax=.3)
-# st.plot(color='ivory',bgcolor='black', orientation="horizontal", linewidth=.5)
-st_all.plot(color='slateblue',bgcolor='linen', orientation="horizontal", linewidth=2)
+
+
+st_all.filter('bandpass', freqmin=0.01, freqmax=.3)
+# st.plot(color='ivory',bgcolor='black', orientation="horizontal", linewidth=.5)#palevioletred
+st_all.plot(outfile='211_high_.png',size=(3000,600),dpi=500,color='brown',bgcolor='white', orientation="horizontal", linewidth=.7)
